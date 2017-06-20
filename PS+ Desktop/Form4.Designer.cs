321e8ae79eea410e742fd1287287cdc38ec5a0e9
spoguru <renatogomes17@msn.com>
2017-06-20 11:42:57 +0100

    update1

--- PS+ Desktop/Form4.Designer.cs
+++ PS+ Desktop/Form4.Designer.cs
@@ -44,7 +44,7 @@
             // button1
             // 
             this.button1.BackgroundImage = global::PS__Desktop.Properties.Resources.bg;
-            this.button1.Location = new System.Drawing.Point(9, 31);
+            this.button1.Location = new System.Drawing.Point(7, 30);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(35, 35);
             this.button1.TabIndex = 3;
